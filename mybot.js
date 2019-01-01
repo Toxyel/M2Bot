3d3e6454ecc9090fd927f4d9385280b88c5f3e6c
Toxyel <toxyel@gmail.com>
2019-01-01 05:44:11 +0100

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -1,7 +1,7 @@
 ﻿const Discord = require("discord.js");
 const bot = new Discord.Client();
 const token = process.env.token;
-const prefix": "§";
+const prefix: "§";
 const ownerID = process.env.author;
 }
 
