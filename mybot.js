a29ce1be234bbbb68129eaa07fdb86895b66400f
Gungnirus <40634915+Gungnirus@users.noreply.github.com>
2019-02-01 16:35:54 +0100

    Update mybot.js

--- mybot.js
+++ mybot.js
@@ -31,6 +31,28 @@ bot.on("message", (message) =>
   if (message.content.startsWith("Čau Martin"))
     message.channel.send("<:god:487604676066410497>");
 
+
+  if(mesage.content.startsWith("§lov"))
+    {
+      var skill = Math.floor(Math.random() * 3);
+      if(skill === 0)
+      {
+        message.channel.send("Smršť meče");
+      }
+      else if(skill === 1)
+      {
+        message.channel.send("Silné tělo");
+      }
+      else if(skill === 2)
+      {
+        message.channel.send("Léčka");
+      }
+      else
+      {
+        message.channel.send("To teda.");
+      }
+    }
+
   if(!message.content.startsWith(prefix) || message.author.bot)
       return;
 
