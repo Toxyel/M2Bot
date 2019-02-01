5b3f4ea8bf94c752ecf9845c8527441656fb0f65
Gungnirus <40634915+Gungnirus@users.noreply.github.com>
2019-02-01 16:50:53 +0100

    Create lov.js

---
+++ commands/lov.js
@@ -0,0 +1,20 @@
+﻿exports.run = (bot, message, args) =>
+{
+      var skill = Math.floor(Math.random() * 3);
+      if(skill === 0)
+      {
+        message.channel.send("Smr�� me�e");
+      }
+      else if(skill === 1)
+      {
+        message.channel.send("Siln� t�lo");
+      }
+      else if(skill === 2)
+      {
+        message.channel.send("L��ka");
+      }
+      else
+      {
+        message.channel.send("To teda.");
+      }
+}
